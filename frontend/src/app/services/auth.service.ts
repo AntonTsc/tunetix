175ed0b110b265bd689039d1aa3f9847fde986f4
RobertRosu <i_rosu@fpzornotza.com>
2025-03-05 21:41:17 +0100

    Servicio auth creado

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl = 'http://localhost/backend/auth';
+  headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
+
+  register(data: any): Observable<any>{
+      return this.http.post(`${this.baseUrl}/register.php`, data, {headers: this.headers})
+  }
+
+  login(data: any): Observable<any>{
+    return this.http.post(`${this.baseUrl}/login.php`, data, {headers: this.headers})
+  }
+
+  logout(){
+    return this.http.get(`${this.baseUrl}/logout.php`, {withCredentials: true})
+    
+  }
+}
