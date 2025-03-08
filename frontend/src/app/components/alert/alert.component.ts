9516081c9aa045eee0fd2d51598532994c614d7c
RobertRosu <i_rosu@fpzornotza.com>
2025-03-08 22:32:55 +0100

    Formulario login funcional

--- frontend/src/app/components/alert/alert.component.ts
+++ frontend/src/app/components/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import ServerResponse from 'src/app/interfaces/ServerResponse';
 
 @Component({
@@ -6,17 +6,10 @@ import ServerResponse from 'src/app/interfaces/ServerResponse';
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css']
 })
-export class AlertComponent implements OnInit{
+export class AlertComponent{
   @Input() serverResponse!: ServerResponse;
   statusStyleColors: Map<string, string> = new Map([
     ['OK', 'text-green-800 bg-green-50 dark:bg-green-600'],
     ['ERROR', 'text-red-800 bg-red-50 dark:bg-red-600']
   ])
-
-  ngOnInit(): void {
-    console.log(this.serverResponse.status)
-    console.log(this.statusStyleColors.get(this.serverResponse.status))
-  }
-
-
 }
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -18,20 +18,9 @@ export class RegisterComponent {
   serverResponse?: ServerResponse;
 
   register(){
-    
-    if(this.repeatPassword.nativeElement.value !== this.form.nativeElement['password'].value){
-      this.serverResponse = {status: 'ERROR', message: 'Las contraseñas no coinciden'}
-      return;
-    }
-
-    if(this.form.nativeElement['first_name'].value === '' || this.form.nativeElement['last_name'].value === '' || this.form.nativeElement['email'].value === '' || this.form.nativeElement['password'].value === ''){
-      this.serverResponse = {status: 'ERROR', message: 'Todos los campos deben ser rellenados'};
-      return;
-
-    }
 
     const fd = new FormData(this.form.nativeElement);
-    
+
     const json = {
       first_name: fd.get('first_name'),
       last_name: fd.get('last_name'),
@@ -39,14 +28,22 @@ export class RegisterComponent {
       password: fd.get('password')
     }
 
+    if(this.repeatPassword.nativeElement.value !== fd.get('password')){
+      this.serverResponse = {status: 'ERROR', message: 'Las contraseñas no coinciden'}
+      return;
+    }
+
+    if(fd.get('first_name') === '' || fd.get('last_name') === '' || fd.get('email') === '' || fd.get('password') === ''){
+      this.serverResponse = {status: 'ERROR', message: 'Todos los campos deben ser rellenados'};
+      return;
+    }
+
     this._auth.register(json).subscribe({
       next: (data: ServerResponse) => {
         this.serverResponse = data;
-        console.log(this.serverResponse);
       },
       error: (err) => {
         this.serverResponse = { status: 'ERROR', message: 'Error en el servidor' };
-        console.error(err);
       }
     });
   }
