7086a20d424c7dca88ab30dcea4b6b6940dd5f18
AntonTsc <a_tschanzshu@fpzornotza.com>
2025-03-20 09:09:42 +0100

    conflictos restantes solucionados

--- frontend/src/app/components/alert/alert.component.ts
+++ frontend/src/app/components/alert/alert.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component, Input, OnInit } from '@angular/core';
-=======
 import { Component, Input } from '@angular/core';
->>>>>>> 1146de4923edd74e91bd9f73d69b5ab024695083
 import ServerResponse from 'src/app/interfaces/ServerResponse';
 
 @Component({
@@ -10,24 +6,10 @@ import ServerResponse from 'src/app/interfaces/ServerResponse';
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css']
 })
-<<<<<<< HEAD
-export class AlertComponent implements OnInit{
-=======
 export class AlertComponent{
->>>>>>> 1146de4923edd74e91bd9f73d69b5ab024695083
   @Input() serverResponse!: ServerResponse;
   statusStyleColors: Map<string, string> = new Map([
     ['OK', 'text-green-800 bg-green-50 dark:bg-green-600'],
     ['ERROR', 'text-red-800 bg-red-50 dark:bg-red-600']
   ])
-<<<<<<< HEAD
-
-  ngOnInit(): void {
-    console.log(this.serverResponse.status)
-    console.log(this.statusStyleColors.get(this.serverResponse.status))
-  }
-
-
-=======
->>>>>>> 1146de4923edd74e91bd9f73d69b5ab024695083
 }
